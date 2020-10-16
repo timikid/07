b150c157796affee49b12777ad478359d0ce8259
timikid <timikit@naver.com>
2020-10-16 23:39:40 +0900

    1

---
+++ ani.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int flag = 1
+	while(flag != 0);
+	{
+      int y;
+      y=3;
+      flag=0;   
+	}
+    y=4; // 변수선언x 
+	
+	return 0;
+	
+}
