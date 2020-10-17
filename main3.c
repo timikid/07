f3382f1dbcb3420654f4563cd796829dfdd2e8d1
timikid <timikit@naver.com>
2020-10-17 17:36:14 +0900

    03

---
+++ main3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main(int argc, char *argv[])
+ {
+	int i=10;
+	printf("함수 호출전 i=%d\n", i);
+	inc(i);
+	printf("함수호출후i=%d\n", i);
+	
+	return 0;
+ }
+  int inc(int counter)
+  {
+  	counter++;
+  	return counter;
+
+  }
